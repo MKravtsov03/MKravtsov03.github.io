fcc364dfd9d8fa59939f9a2d45b13b758a72ce6a
mac <mkravtsov03@gmail.com>
2023-02-02 11:23:07 +0100

    commit

--- product.js
+++ product.js
@@ -4139,7 +4139,7 @@ unlayer.registerPropertyEditor({
             const fieldItems = node.querySelectorAll('.fields-item');
             fieldItems.forEach(function(item) {
                 item.querySelector('.delete-field').onclick = function() {
-                    console.log(this.dataset.value)
+                    console.log(item.dataset.value)
                 }
             })
         }
