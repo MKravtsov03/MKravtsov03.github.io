4931be44b668fb97ae8d8e2511b4d7856cf9700b
mac <mkravtsov03@gmail.com>
2023-02-10 11:44:17 +0100

    commit

--- product.js
+++ product.js
@@ -4167,6 +4167,55 @@ const additionalFields = (values) => {
     </div>
 `}
 
+const emaiPhoneEdit = (values) => {
+    return `
+        <div class="email-phone-edit">
+            <div class="blockbuilder-widget-label">
+                <p class="blockbuilder-label-primary">Email/Phone field edit</p>
+            </div>
+            <button class="edit-field">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pencil" class="svg-inline--fa fa-pencil " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path></svg>
+            </button>
+            <div class="edit-popup ${values?.editPopup.active ? 'active' : ''}">
+            <form class="edit-form">
+                <div class="edit-form__title">
+                    Update field
+                </div>
+                <div class="form-item">
+                     <span class="form-row">
+                     <label for="label_text">
+                          <p>Label text</p>
+                     </label>
+                     <label>
+                        <input ${values?.fieldsProps.label ? 'checked' : ''} type="checkbox" name="label">
+                        <span>Show label</span>
+                      </label>
+                     </span>
+                   
+                     <input value="${values?.fieldsProps.label_text}" type="text" name="label_text" id="label_text" class="form-control">
+                 </div>
+                 
+                <div class="form-item">
+                    <label>
+                        <p>Placeholder text</p>
+                        <input value="${values?.fieldsProps.placeholder_text}" type="text" name="placeholder_text" class="form-control">
+                    </label>
+                 </div>
+                 
+                 <div class="edit-form__footer">
+                    <button type="submit" class="edit-form__save">
+                        Update field
+                    </button>
+                    <button class="edit-form__cancel">
+                        Cancel
+                    </button>
+                </div>
+            </form>
+        </div>
+        </div>   
+    `
+}
+
 unlayer.registerPropertyEditor({
     name: 'additional_form_fields',
     layout: 'bottom',
@@ -4210,6 +4259,39 @@ unlayer.registerPropertyEditor({
     })
 });
 
+
+unlayer.registerPropertyEditor({
+    name: 'emailPhoneEdit',
+    layout: 'bottom',
+    Widget: unlayer.createWidget({
+        render( value, updateValue, data) {
+            return emaiPhoneEdit(value)
+        },
+        mount(node, value, updateValue) {
+
+            node.querySelector('.edit-field').onclick = function() {
+                return updateValue({...value, editPopup: true})
+            }
+
+            const popupClose = node.querySelector('.edit-form__cancel');
+            popupClose.onclick = function () {
+                return updateValue({...value, editPopup: false})
+            }
+
+            const form = node.querySelector('.edit-form');
+            form.onsubmit = function (e) {
+                e.preventDefault();
+                const data = new FormData(form)
+                let formValues = {};
+                for (let [key, value] of data) {
+                    formValues[key] = value
+                }
+                return updateValue({ fieldsProps: {...formValues},  editPopup: false })
+            }
+        }
+    })
+});
+
 const additionalFieldsRenderer = {
     first_name: (values) => `
         <div style="margin-bottom: ${values.fieldsGap}px" class="custom-form__item">
@@ -4242,15 +4324,15 @@ const getFormTemplate = () => function(values) {
                 ${values?.email_phone == 'email' ? 
                 `
                     <div style="margin-bottom: ${values.fieldsGap}px" class="custom-form__item">
-                        ${values?.email_phone_label ? `<label style="font-family: ${values?.labelFont.value}; font-size: ${values?.labelFontSize}px; color: ${values?.labelColor}" for="email">${values?.email_phone_label_text}</label>` : ''}
-                        <input required class="custom-form__field" placeholder="Enter your email" type="email" name="email" id="email" />
+                        ${values?.email_phone_props.fieldProps.label ? `<label style="font-family: ${values?.labelFont.value}; font-size: ${values?.labelFontSize}px; color: ${values?.labelColor}" for="email">${values?.email_phone_props.fieldProps.label_text}</label>` : ''}
+                        <input required class="custom-form__field" placeholder="${values?.email_phone_props.fieldProps.placeholder_text}" type="email" name="email" id="email" />
                     </div>
                 ` 
                 : 
                 `
                     <div style="margin-bottom: ${values.fieldsGap}px" class="custom-form__item">
-                        ${values?.email_phone_label ? `<label style="font-family: ${values?.labelFont.value}; font-size: ${values?.labelFontSize}px; color: ${values?.labelColor}" for="phone">${values?.email_phone_label_text}</label>` : ''}
-                        <input required class="custom-form__field" placeholder="Enter your phone" type="tel" name="phone" id="phone" />
+                        ${values?.email_phone_props.fieldProps.label ? `<label style="font-family: ${values?.labelFont.value}; font-size: ${values?.labelFontSize}px; color: ${values?.labelColor}" for="phone">${values?.email_phone_props.fieldProps.label_text}</label>` : ''}
+                        <input required class="custom-form__field" placeholder="${values?.email_phone_props.fieldProps.placeholder_text}" type="tel" name="phone" id="phone" />
                     </div>
                 `
                 }
@@ -4361,10 +4443,17 @@ unlayer.registerTool({
                     defaultValue: false,
                     widget: 'toggle',
                 },
-                email_phone_label_text: {
-                    label: 'Email/phone field label text',
-                    defaultValue: 'Email',
-                    widget: 'text',
+                email_phone_props: {
+                    label: 'Email/phone field edit',
+                    defaultValue: {
+                        fieldProps: {
+                                label: false,
+                                label_text: 'Email',
+                                placeholder_text: 'Enter your email'
+                        },
+                        editPopup: false
+                    },
+                    widget: 'emailPhoneEdit',
                 },
                 fieldsBorder: {
                     label: 'Fields border',
