962ada5035d08c6a8ef7a2fc8c4a337d4ae2f396
mac <mkravtsov03@gmail.com>
2023-04-07 11:24:27 +0200

    commit

--- product.js
+++ product.js
@@ -3598,12 +3598,12 @@ const reviewSelect = (value, data) => {
 
     const reviewsList = searchFiltering(productFiltering(filteredReviews, productFilter), searchString)
 
-    const products = reviews.product_reviews.reduce((acc, product) => {
+    const products = reviews?.product_reviews?.reduce((acc, product) => {
         if (!acc.find(prod => prod.productId == product.yotpo_product_id)) {
             acc.push({productId: product.yotpo_product_id, productName: product.product_name});
         }
         return acc;
-    }, [])
+    }, []) || []
 
     return `
 
